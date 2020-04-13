59cee2c961d02edbcc7a86682d748e599b678ac3
Cabu Vallejos <cabupy@gmail.com>
2020-04-13 14:27:41 -0400

    Modificamos objeto de respuesta

--- routes/geoinversa.js
+++ routes/geoinversa.js
@@ -4,13 +4,17 @@ const db = require('../db')
 
 router.get('/:lat/:lng', async (req, res, next) => {
   const sqlSelect = `SELECT * FROM public.geo_inversa_loc(${req.params.lat}, ${req.params.lng})`
-  //console.log(sqlSelect)
-  const rows = await db.query(sqlSelect)
-  if (rows.length) {
-    res.status(200).json({ success: true, data: rows, rowsCount: rows.length })
-  } else {
-    res.status(404).json({ success: false, data: [], rowsCount: rows.length })
+  try {
+    const rows = await db.query(sqlSelect)
+    if (rows.length) {
+      res.status(200).json({ success: true, data: rows[0], ip: req.ip })
+    } else {
+      res.status(404).json({ success: false, data: {}, message: 'No se obtuvieron resultados.' })
+    } 
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Error al buscar la dirección.' })  
   }
+  
 })
 
 module.exports = router
