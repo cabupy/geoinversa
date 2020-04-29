bb9a7cc5e20d73ba2286a5686ed1a64e18244088
Cabu Vallejos <cabupy@gmail.com>
2020-04-28 21:51:54 -0400

    Dividimos el metodo GET geoinversa en 3 rutas diferentes para AR

--- routes/geoinversa.js
+++ routes/geoinversa.js
@@ -20,17 +20,65 @@ router.get('/:lat/:lng', async (req, res, next) => {
 })
 
 // Para Argentina por vos. Ejemplo: http://localhost:33000/v1/geoinversa/ar/-58.382675/-34.603728
+// devuelve poligonos y lineas
 router.get('/ar/:lng/:lat', async (req, res, next) => {
   // bd osm_ar
-  const sqlSelect = `SELECT * FROM vamyal.getAddress(${req.params.lng}, ${req.params.lat})`
+  const sqlSelectAddress = `SELECT * FROM vamyal.getAddress(${req.params.lng}, ${req.params.lat})`
+  const sqlSelectPolygons = `SELECT * FROM vamyal.getPolygons(${req.params.lng}, ${req.params.lat})`
   try {
-    const rows = await db.query(sqlSelect)
-    if (rows.length) {
-      res.status(200).json({ success: true, data: rows[0], ip: req.ips })
+    const rowsA = await db.query(sqlSelectAddress)
+    const rowsP = await db.query(sqlSelectPolygons)
+    if (rowsA.length || rowsA.length) {
+      res.status(200).json(
+        { 
+          success: true, 
+          data: { 
+            address: (rowsA[0].getaddress ? rowsA[0].getaddress : ''), 
+            polygons: (rowsP[0].getpolygons ? rowsP[0].getpolygons : '')
+          }, 
+          ip: req.ips 
+        })
     } else {
       res.status(404).json({ success: false, data: {}, message: 'No se obtuvieron resultados.' })
     } 
   } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ success: false, message: 'Error al buscar la dirección.' })  
+  }
+  
+})
+
+// solo lineas
+router.get('/ar/lines/:lng/:lat', async (req, res, next) => {
+  // bd osm_ar
+  const sqlSelectAddress = `SELECT * FROM vamyal.getAddress(${req.params.lng}, ${req.params.lat})`
+  try {
+    const rowsA = await db.query(sqlSelectAddress)
+    if (rowsA.length) {
+      res.status(200).json({ success: true, data: { address : rowsA[0].getaddress }, ip: req.ips })
+    } else {
+      res.status(404).json({ success: false, data: {}, message: 'No se obtuvieron resultados.' })
+    } 
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ success: false, message: 'Error al buscar la dirección.' })  
+  }
+  
+})
+
+// solo poligonos
+router.get('/ar/polygons/:lng/:lat', async (req, res, next) => {
+  // bd osm_ar
+  const sqlSelectPolygons = `SELECT * FROM vamyal.getPolygons(${req.params.lng}, ${req.params.lat})`
+  try {
+    const rowsP = await db.query(sqlSelectPolygons)
+    if (rowsP.length) {
+      res.status(200).json({ success: true, data: { polygons: rowsP[0].getpolygons }, ip: req.ips })
+    } else {
+      res.status(404).json({ success: false, data: {}, message: 'No se obtuvieron resultados.' })
+    } 
+  } catch (error) {
+    console.log(error.message)
     res.status(500).json({ success: false, message: 'Error al buscar la dirección.' })  
   }
   
